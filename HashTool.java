caad27aef7191d6db207bca73c11e3350a040024
sereneshree <mishu.08nov23@gmail.com>
2026-05-08 23:24:30 +0530

    Added HashMap Code

---
+++ HashTool.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class HashTool {
+    public static void main(String a[]){
+
+        int [] arr={34,56,820,110,78,34,820,34};
+
+        HashMap < Integer,Integer > map=new HashMap<>();
+
+        //counting frequency
+        for (int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        // printing result
+        for(int key:map.keySet()){
+            System.out.println(key+" "+"appears"+ map.get(key)+" "+"times");
+        }
+    }    
+}
--- Hashing.java
+++ Hashing.java
@@ -2,19 +2,16 @@ import java.util.*;
 public class Hashing{
     public static void main(String a[]){
         
-        int [] sums={1,2,3,2,2,3,1,4,6,7};
-        int max=10;
-        int [] hash=new int[max+1];
+        int [] sums={1,2,3,2,2,3,1,4,6,7};  // given array
+        int max=10; //fixed size. the max no. of this array can't exceed more than 10
+        int [] hash=new int[max+1]; // count box. will keep the count of number each time it is found in the array
 
-        for (int i=0;i<sums.length;i++){
-            hash[sums[i]]++;
+        for (int i=0;i<sums.length;i++){ // iterates through the array 
+            hash[sums[i]]++;  // increase and stores the count everytime it encounter the value
         }
 
-        for(int i=0;i<hash.length;i++){
-            System.out.println(i+" "+"appears" +" "+ hash[i]+" "+"times");
+        for(int i=0;i<hash.length;i++){ // iterates through the hash array which contains the count of each elements 
+            System.out.println(i+" "+"appears" +" "+ hash[i]+" "+"times");  //prints the count of each element
         }
-        
-
     }
-
 }
